8e913ead79cdefeaff15f9e8181327aa50f62edb
mona-baharlou <mona.baharlou755@gmail.com>
2024-05-24 00:27:57 +0330

    SecondActivity.xml updated

--- app/src/main/java/com/baharlou/sampleneumorphism/MainActivity.kt
+++ app/src/main/java/com/baharlou/sampleneumorphism/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.baharlou.sampleneumorphism
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.baharlou.sampleneumorphism.databinding.ActivityMainBinding
+import soup.neumorphism.NeumorphImageView
+import soup.neumorphism.ShapeType
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -18,9 +22,72 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @SuppressLint("RestrictedApi")
     private fun buttonClick() {
         binding.btnGo.setOnClickListener {
             goToSecondActivity()
+        }
+
+        binding.neumorphImageView7.setOnClickListener {
+
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView8.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView8.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+
+            }
+
+        }
+
+        binding.neumorphImageView8.setOnClickListener {
+
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView7.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView7.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+
+            }
+
         }
     }
 
--- app/src/main/java/com/baharlou/sampleneumorphism/SecondActivity.kt
+++ app/src/main/java/com/baharlou/sampleneumorphism/SecondActivity.kt
@@ -1,10 +1,15 @@
 package com.baharlou.sampleneumorphism
 
-import android.opengl.Visibility
+import android.annotation.SuppressLint
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.baharlou.sampleneumorphism.databinding.ActivitySecondBinding
+import soup.neumorphism.NeumorphImageView
+import soup.neumorphism.ShapeType
+import soup.neumorphism.ShapeType.Companion.PRESSED
 
 class SecondActivity : AppCompatActivity() {
 
@@ -16,6 +21,229 @@ class SecondActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setContentsOfCards()
+        setClicks()
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun setClicks() {
+        binding.neumorphImageView.setOnClickListener {
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView2.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView3.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView4.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView2.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView3.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView4.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            }
+
+        }
+        binding.neumorphImageView2.setOnClickListener {
+
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView3.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView4.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView3.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView4.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            }
+
+        }
+        binding.neumorphImageView3.setOnClickListener {
+
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView2.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView4.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView2.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView4.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            }
+
+        }
+        binding.neumorphImageView4.setOnClickListener {
+
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView2.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView3.setShapeType(ShapeType.FLAT)
+                binding.neumorphImageView.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView2.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView3.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+                binding.neumorphImageView.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            }
+
+        }
+
+
+        binding.neumorphImageView5.setOnClickListener {
+
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView6.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView6.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+
+            }
+
+        }
+
+        binding.neumorphImageView6.setOnClickListener {
+
+            val shapeType = (it as NeumorphImageView).getShapeType()
+            if (shapeType == ShapeType.PRESSED) {
+
+                it.setShapeType(ShapeType.FLAT)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+            } else {
+
+                it.setShapeType(ShapeType.PRESSED)
+                it.setColorFilter(
+                    ContextCompat.getColor(this, R.color.blue_light),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+                binding.neumorphImageView5.setShapeType(ShapeType.FLAT)
+
+                binding.neumorphImageView5.setColorFilter(
+                    ContextCompat.getColor(this, R.color.icon_default_color),
+                    android.graphics.PorterDuff.Mode.SRC_IN
+                )
+
+
+            }
+
+        }
     }
 
     private fun setContentsOfCards() {
@@ -30,7 +258,6 @@ class SecondActivity : AppCompatActivity() {
         binding.directionCard.txtOne.text = "Direction"
         binding.directionCard.txtSec.text = "Automatic Mode"
         binding.directionCard.imgChecking.visibility = View.INVISIBLE
-
 
 
     }
